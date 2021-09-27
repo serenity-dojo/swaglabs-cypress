de63128361333e061a2177bcc39468fabc6ea117
John Ferguson Smart <john.smart@wakaleo.com>
2021-09-27 22:14:03 +0100

    Sample code

--- cypress/integration/1-logging-on/login.spec.js
+++ cypress/integration/1-logging-on/login.spec.js
@@ -1,54 +1,38 @@
 /// <reference types='cypress'/>
 
-beforeEach(() => {
-    cy.visit('https://www.saucedemo.com/')
-});
+import { LoginPage } from "../../page-objects/login-page";
+import { PageHeader } from "../../page-objects/page-header";
 
 describe('When logging on', () => {
+
+    const loginPage = new LoginPage()
+    const pageHeader = new PageHeader()
+
+    beforeEach(() => {
+        loginPage.open()
+    });
+    
     describe('with a valid user account', () => {
-        it('Entering valid credentials should take the user to the product page', () => {
-            cy.get("[data-test='username']").type("standard_user");
-            cy.get("[data-test='password']").type("secret_sauce");
-
-            cy.get("[data-test='login-button']").click()
-
-            cy.get('.title').should('have.text', 'Products')
+        it('With valid credentials', () => {
+            loginPage.loginWith("standard_user","secret_sauce")
+            pageHeader.title().should('have.text', 'Products')
         })
 
-        it('And an incorrect username', () => {
-            cy.get("[data-test='username']").type("WRONG_USER");
-            cy.get("[data-test='password']").type("secret_sauce");
-
-            cy.get("[data-test='login-button']").click()
-
-            cy.get('[data-test=error]')
-              .should('be.visible')
-              .should('contain', 'Username and password do not match any user')
+        it('With an incorrect username', () => {
+            loginPage.loginWith("WRONG_USER","secret_sauce")
+            loginPage.errorMessage().should('contain', 'Username and password do not match any user')
         })
 
-        it('And an incorrect password', () => {
-            cy.get("[data-test='username']").type("standard_user");
-            cy.get("[data-test='password']").type("WRONG-PASSWORD");
-
-            cy.get("[data-test='login-button']").click()
-
-            cy.get('[data-test=error]')
-              .should('be.visible')
-              .should('contain', 'Username and password do not match any user')
+        it('With incorrect password', () => {
+            loginPage.loginWith("standard_user","WRONG_USER")
+            loginPage.errorMessage().should('contain', 'Username and password do not match any user')
         })
     });
 
     describe('with a blocked account', () => {
         it('should not allow a blocked user to login', () => {
-            cy.get("[data-test='username']").type("locked_out_user");
-            cy.get("[data-test='password']").type("secret_sauce");
-
-            cy.get("[data-test='login-button']").click()
-
-            cy.get('[data-test=error]')
-              .should('be.visible')
-              .should('contain', 'Sorry, this user has been locked out.')   
+            loginPage.loginWith("locked_out_user","secret_sauce")
+            loginPage.errorMessage().should('contain', 'Sorry, this user has been locked out.')   
         })
     })
-
 })
